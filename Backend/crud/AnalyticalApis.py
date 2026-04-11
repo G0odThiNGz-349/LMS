badded1064024f46fa5b11c9886f89e99f0c6e5d
G0odThiNGz-349 <goodthingz349@gmail.com>
2026-04-11 14:22:59 +0200

    Add CRUD functions for students, attendance, course offerings, and enrollments

---
+++ Backend/crud/AnalyticalApis.py
@@ -0,0 +1,24 @@
+from sqlalchemy.orm import Session
+from Backend.models import Student, Attendance, CourseOffering, Enrollment
+
+def get_students(db: Session, page: int , page_size: int ):
+    query = db.query(Student)
+    return query.offset((page - 1) * page_size).limit(page_size).all()
+
+
+
+def get_attendance(db: Session, page: int , page_size: int):
+    query = db.query(Attendance)
+    return query.offset((page - 1) * page_size).limit(page_size).all()
+
+
+
+def get_course_offering(db: Session, page: int, page_size: int):
+    query = db.query(CourseOffering)
+    return query.offset((page - 1) * page_size).limit(page_size).all()
+
+
+
+def get_enrollments(db: Session, page: int, page_size: int):
+    query = db.query(Enrollment)
+    return query.offset((page - 1) * page_size).limit(page_size).all()
