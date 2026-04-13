ca80fb6a98547b447c978375e4662749e5a39320
G0odThiNGz-349 <goodthingz349@gmail.com>
2026-04-13 16:58:56 +0200

    Add Course and Department models to AnalyticalApis; update function signatures for consistency

--- Backend/crud/AnalyticalApis.py
+++ Backend/crud/AnalyticalApis.py
@@ -1,13 +1,13 @@
 from sqlalchemy.orm import Session
-from Backend.models import Student, Attendance, CourseOffering, Enrollment
+from Backend.models import Student, Attendance, CourseOffering, Enrollment, Course, Department
 
-def get_students(db: Session, page: int , page_size: int ):
+def get_students(db: Session, page: int, page_size: int):
     query = db.query(Student)
     return query.offset((page - 1) * page_size).limit(page_size).all()
 
 
 
-def get_attendance(db: Session, page: int , page_size: int):
+def get_attendance(db: Session, page: int, page_size: int):
     query = db.query(Attendance)
     return query.offset((page - 1) * page_size).limit(page_size).all()
 
@@ -21,4 +21,14 @@ def get_course_offering(db: Session, page: int, page_size: int):
 
 def get_enrollments(db: Session, page: int, page_size: int):
     query = db.query(Enrollment)
+    return query.offset((page - 1) * page_size).limit(page_size).all()
+
+
+def get_department(db: Session, page: int, page_size: int):
+    query = db.query(Department)
+    return query.offset((page - 1) * page_size).limit(page_size).all()
+
+
+def get_course(db: Session, page: int, page_size: int):
+    query = db.query(Course)
     return query.offset((page - 1) * page_size).limit(page_size).all()
