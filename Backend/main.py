d5acb22ff804e416b6b0004a99ce41d88a994c1e
G0odThiNGz-349 <goodthingz349@gmail.com>
2026-04-13 16:59:28 +0200

    Initialize FastAPI application and include routers for various endpoints

---
+++ Backend/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from Backend.database import engine
+from Backend.models import Base
+from Backend.routers import combiend, department, course , AnalyticalApis
+
+app = FastAPI()
+
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(combiend.router)
+app.include_router(department.router)
+app.include_router(course.router)
+app.include_router(AnalyticalApis.student_router)
+app.include_router(AnalyticalApis.attendance_router)
+app.include_router(AnalyticalApis.course_offering_router)
+app.include_router(AnalyticalApis.enrollment_router)
+app.include_router(AnalyticalApis.department_router)
+app.include_router(AnalyticalApis.course_router)
