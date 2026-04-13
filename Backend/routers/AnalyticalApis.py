fceca10c5a7e095fdfab9cd83d398636493ca867
G0odThiNGz-349 <goodthingz349@gmail.com>
2026-04-13 16:59:11 +0200

    Refactor AnalyticalApis to standardize export routes; add course and department endpoints

--- Backend/routers/AnalyticalApis.py
+++ Backend/routers/AnalyticalApis.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter, Depends, Query
 from sqlalchemy.orm import Session
 from Backend.database import get_db
-from Backend.schemas.AnalyticalAPis import PaginatedResponse, ExportAttendance, ExportStudent, ExportCourseOffering, ExportEnrollment
-from Backend.crud.AnalyticalApis import get_students, get_attendance, get_course_offering, get_enrollments
+from Backend.schemas.AnalyticalAPis import PaginatedResponse, ExportAttendance, ExportStudent, ExportCourseOffering, ExportEnrollment, ExportCourse, ExportDepartment
+from Backend.crud.AnalyticalApis import get_students, get_attendance, get_course_offering, get_enrollments, get_course, get_department
 
 
 
 
-student_router = APIRouter(prefix="/students", tags=["Students"])
+student_router = APIRouter(prefix="/export_students", tags=["Students"])
 
 @student_router.get("/", response_model=PaginatedResponse[ExportStudent])
 def list_students(page: int = Query(1, ge=1), page_size: int = Query(10, ge=1, le=100), db: Session = Depends(get_db)
@@ -23,44 +23,74 @@ def list_students(page: int = Query(1, ge=1), page_size: int = Query(10, ge=1, l
 
 
 
-enrollment_router = APIRouter(prefix="/enrollments", tags=["Enrollments"])
+enrollment_router = APIRouter(prefix="/export_enrollments", tags=["Enrollments"])
 
 @enrollment_router.get("/", response_model=PaginatedResponse[ExportEnrollment])
 def list_enrollments(page: int = Query(1, ge=1), page_size: int = Query(10, ge=1, le=100), db: Session = Depends(get_db)):
-    students = get_enrollments(db, page, page_size)
+    enrollments = get_enrollments(db, page, page_size)
 
     return {
         "page": page,
         "page_size": page_size,
-        "data": students
+        "data": enrollments
     }
 
 
 
 
-attendance_router = APIRouter(prefix="/attendance", tags=["Attendance"])
+attendance_router = APIRouter(prefix="/export_attendance", tags=["Attendance"])
 
 @attendance_router.get("/", response_model=PaginatedResponse[ExportAttendance])
 def list_attendance(page: int = Query(1, ge=1), page_size: int = Query(10, ge=1, le=100), db: Session = Depends(get_db)):
-    students = get_attendance(db, page, page_size)
+    attendance = get_attendance(db, page, page_size)
 
     return {
         "page": page,
         "page_size": page_size,
-        "data": students
+        "data": attendance
     }
 
 
 
 
-course_offering_router = APIRouter(prefix="/course_offerings", tags=["CourseOffering"])
+course_offering_router = APIRouter(prefix="/export_course_offerings", tags=["CourseOffering"])
 
 @course_offering_router.get("/", response_model=PaginatedResponse[ExportCourseOffering])
 def list_course_offerings(page: int = Query(1, ge=1), page_size: int = Query(10, ge=1, le=100), db: Session = Depends(get_db)):
-    students = get_course_offering(db, page, page_size)
+    course_offering = get_course_offering(db, page, page_size)
 
     return {
         "page": page,
         "page_size": page_size,
-        "data": students
+        "data": course_offering
+    }
+
+
+
+
+course_router = APIRouter(prefix="/export_courses", tags=["Course"])
+
+@course_router.get("/", response_model=PaginatedResponse[ExportCourse])
+def list_courses(page: int = Query(1, ge=1), page_size: int = Query(10, ge=1, le=100), db: Session = Depends(get_db)):
+    course = get_course(db, page, page_size)
+
+    return {
+        "page": page,
+        "page_size": page_size,
+        "data": course
+    }
+
+
+
+
+department_router = APIRouter(prefix="/export_department", tags=["Department"])
+
+@department_router.get("/", response_model=PaginatedResponse[ExportDepartment])
+def list_departments(page: int = Query(1, ge=1), page_size: int = Query(10, ge=1, le=100), db: Session = Depends(get_db)):
+    department = get_department(db, page, page_size)
+
+    return {
+        "page": page,
+        "page_size": page_size,
+        "data": department
     }
