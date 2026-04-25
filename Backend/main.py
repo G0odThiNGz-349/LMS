14cbd7cf4c1720343a4099c2b9220c27a74b8060
G0odThiNGz-349 <goodthingz349@gmail.com>
2026-04-25 18:16:46 +0300

    Add additional routers for ticket, course offerings, attendance, enrollments, course prerequisites, and academic semesters

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from Backend.database import engine
 from Backend.models import Base
-from Backend.routers import combiend, department, course , AnalyticalApis
+from Backend.routers import combiend, department, course , AnalyticalApis, ticket, coursePrerequisite, enrollment, attendance, academicSemester, courseOffering
 
 app = FastAPI()
 
@@ -11,6 +11,12 @@ Base.metadata.create_all(bind=engine)
 app.include_router(combiend.router)
 app.include_router(department.router)
 app.include_router(course.router)
+app.include_router(ticket.router)
+app.include_router(courseOffering.router)
+app.include_router(attendance.router)
+app.include_router(enrollment.router)
+app.include_router(coursePrerequisite.router)
+app.include_router(academicSemester.router)
 app.include_router(AnalyticalApis.student_router)
 app.include_router(AnalyticalApis.attendance_router)
 app.include_router(AnalyticalApis.course_offering_router)
