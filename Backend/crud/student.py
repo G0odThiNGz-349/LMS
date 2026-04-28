3fa4023054e90ff1daa6fab340021d5c51f24145
G0odThiNGz-349 <goodthingz349@gmail.com>
2026-04-28 23:57:47 +0300

    Add get_current_student function to retrieve current student's details

--- Backend/crud/student.py
+++ Backend/crud/student.py
@@ -1,6 +1,8 @@
 from sqlalchemy.orm import Session, joinedload
 from Backend.models import Student, User
 from Backend.schemas.student import StudentCreate, StudentAdminUpdate
+from Backend.auth.dep import get_current_user
+from fastapi import Depends
 
 
 def create_student(db: Session, student: StudentCreate):
@@ -12,12 +14,33 @@ def create_student(db: Session, student: StudentCreate):
     
     return db_student
 
+
 def get_student(db: Session, university_id: str):
     return db.query(Student).options(
         joinedload(Student.user)
     ).join(Student.user).filter(
         User.university_id == university_id
     ).first()
+
+
+def get_current_student(db: Session, current_user: User = Depends(get_current_user)):
+    current_student = db.query(Student).options(
+        joinedload(Student.user)
+    ).filter(Student.user_id == current_user.id).first()
+
+    return{
+        "full_name": current_student.full_name,
+        "national_id": current_student.national_id,
+        "phone": current_student.phone,
+        "birth_date": current_student.birth_date,
+        "address": current_student.address,
+        "enroll_date": current_student.enroll_date,
+        "expected_graduation": current_student.expected_graduation,
+        "academic_year": current_student.academic_year,
+        "current_gpa": current_student.current_gpa,
+        "university_id": current_student.user.university_id,
+        "email": current_student.user.email,
+    }
 
 
 def get_students(db: Session, skip: int = 0, limit: int = 100):
